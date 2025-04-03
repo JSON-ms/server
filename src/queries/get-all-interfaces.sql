988176aac5d3d8790d9b5c05a333f37a1925a672
Danny Coulombe <danny.coulombe@yolarx.com>
2025-04-03 08:54:31 -0400

    fix permissions

--- src/queries/get-all-interfaces.sql
+++ src/queries/get-all-interfaces.sql
@@ -5,21 +5,22 @@ SELECT
     'owner' AS type,
     u.name AS owner_name
 FROM interfaces AS i
-         INNER JOIN users AS u ON u.id = i.created_by
-         LEFT JOIN permissions AS pa ON pa.interface_uuid = i.uuid AND pa.type = 'admin'
-         LEFT JOIN permissions AS pi ON pi.interface_uuid = i.uuid AND pi.type = 'interface'
+     INNER JOIN users AS u ON u.id = i.created_by
+     LEFT JOIN permissions AS pa ON pa.interface_uuid = i.uuid AND pa.type = 'admin'
+     LEFT JOIN permissions AS pi ON pi.interface_uuid = i.uuid AND pi.type = 'interface'
 WHERE i.created_by = :userId
 GROUP BY i.uuid
 
-UNION
+UNION ALL
 
 SELECT i.*,
        null AS permission_interface,
        null AS permission_admin,
-       p.type,
+       GROUP_CONCAT(DISTINCT(p.type)) as type,
        owner.name AS owner_name
 FROM permissions AS p
-INNER JOIN users AS u ON u.email = p.email
-INNER JOIN interfaces AS i ON i.uuid = p.interface_uuid
-INNER JOIN users AS owner ON owner.id = i.created_by
+    INNER JOIN users AS u ON u.email = p.email
+    INNER JOIN interfaces AS i ON i.uuid = p.interface_uuid
+    INNER JOIN users AS owner ON owner.id = i.created_by
 WHERE u.id = :userId
+GROUP BY i.uuid
