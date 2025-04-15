280b9a13d6196828cc49a1488bfd45abcc46a007
Danny Coulombe <danny.coulombe@yolarx.com>
2025-04-15 09:32:10 -0400

    can't access webhook when shared

--- src/queries/get-all-webhooks.sql
+++ src/queries/get-all-webhooks.sql
@@ -1,5 +1,16 @@
-SELECT
-    w.*
-FROM webhooks AS w
-WHERE
-    w.created_by = :userId
+SELECT DISTINCT combined_results.uuid, combined_results.*
+FROM (
+         SELECT w.*
+         FROM webhooks AS w
+         WHERE w.created_by = :userId
+
+         UNION ALL
+
+         SELECT w.*
+         FROM permissions AS p
+                  INNER JOIN users AS u ON u.email = p.email
+                  INNER JOIN interfaces AS i ON i.uuid = p.interface_uuid
+                  INNER JOIN users AS owner ON owner.id = i.created_by
+                  LEFT JOIN webhooks AS w ON w.uuid = i.webhook
+         WHERE u.id = :userId
+     ) AS combined_results
